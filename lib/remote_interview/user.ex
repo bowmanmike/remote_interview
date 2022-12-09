3e09a84a2f64de8a8893f03d5482818048161188
Mike Bowman <mike@mikebowman.dev>
2022-12-09 16:36:36 -0500

    Add user schema

---
+++ lib/remote_interview/user.ex
@@ -0,0 +1,18 @@
+defmodule RemoteInterview.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :points, :integer
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:points])
+    |> validate_required([:points])
+    |> validate_number(:points, greater_than_or_equal_to: 0, less_than_or_equal_to: 100)
+  end
+end
